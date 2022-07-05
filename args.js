5319cabf1c64ad90c27c33148c345149cec6a208
nmsteve <nmsteve45@gmail.com>
2022-07-05 21:06:17 +0300

    Verify Neon PET collection

---
+++ args.js
@@ -0,0 +1,8 @@
+
+
+module.exports = [
+  "https://ipfs.io/ipfs/QmYzEtYVeVRr9RwuXXcif3TaozM6QXfdwPzSCPK33kQz6j/",
+  'NEON PET Collection',
+  'N-PET',
+   '0xdd305DCf8C019B2E89cCEcfeDD80093726F611EF'
+]
